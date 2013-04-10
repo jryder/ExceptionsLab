2d1409515af36f9e34722359afffa17267a4ec0d
jorda_000 <jorda_000@192.168.0.102>
2013-04-10 00:25:30 -0500

    Done

---
+++ src/lab4/InputOutputGui.java
@@ -0,0 +1,46 @@
+package lab4;
+
+import javax.swing.JOptionPane;
+
+/**
+ * This class is responsible for all input and output in the program.
+ * 
+ * @author  Jim Lombardo, jlombardo@wctc.edu
+ * @version 1.00
+ */
+public class InputOutputGui {
+    private NameService nameService;
+
+    public InputOutputGui() {
+        nameService = new NameService();
+    }
+
+    public void startConversation() {
+        
+        String fullName = JOptionPane.showInputDialog("Enter full name:");
+        String lastName = "";
+        String msg = "";
+        
+	try {
+	    lastName = nameService.extractLastName(fullName);
+	} catch (Exception m) {
+	    msg = "Please enter a valid full name";
+	}
+finally{
+	    System.out.println("The startConversion method has been run");
+	}
+	
+	
+
+	if (msg.equals("")) {
+	    msg = "Your last name is: " + lastName;
+	};
+
+	JOptionPane.showMessageDialog(null, msg);
+
+	
+	
+	
+    }
+     
+}
